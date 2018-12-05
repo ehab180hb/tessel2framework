10e8a1ade2e254ee3769efeb75817b3cfb4b4dd6
Ehab Khaireldin <ehabkhaireldin@gmail.com>
2018-12-05 20:03:12 +0100

    init tests fixed

--- src/test/main/init.test.ts
+++ src/test/main/init.test.ts
@@ -1,24 +1,22 @@
-// import { attachScreenUpdate } from '../../lcd/lcd.events'
-// import { boardEvents, BoardEvents } from '../../board/board.events'
-// import { initComponents } from '../../init'
+import { attachScreenUpdate } from '../../lcd/lcd.events'
+import { boardEvents, BoardEvents } from '../../board/board.events'
+import { initComponents } from '../../init'
+import { initDefaultButtons } from '../../button/button.handler'
 
-// describe('init tests', () => {
-//   beforeEach(() => {
-//     ;(attachScreenUpdate as any) = jest.fn()
-//     ;(attachButtonUpdate as any) = jest.fn()
-//   })
+describe('init tests', () => {
+  beforeEach(() => {
+    ;(attachScreenUpdate as any) = jest.fn()
+    ;(initDefaultButtons as any) = jest.fn()
+  })
 
-//   test('Screen update methods attacher is called', () => {
-//     initComponents()
-//     boardEvents.emit(BoardEvents.UPDATE_SCREEN, 'x')
-//     expect(attachScreenUpdate).toBeCalledWith('x')
-//   })
+  test('Screen update methods attacher is called', () => {
+    initComponents()
+    boardEvents.emit(BoardEvents.UPDATE_SCREEN, 'x')
+    expect(attachScreenUpdate).toBeCalledWith('x')
+  })
 
-//   test('Button update methods attacher is called', () => {
-//     initComponents()
-//     boardEvents.emit(BoardEvents.UPDATE_BUTTONS, 'x')
-//     expect(attachButtonUpdate).toBeCalledWith('x')
-//   })
-// })
-
-test('x', () => {})
+  test('Button update methods attacher is called', () => {
+    initComponents()
+    expect(initDefaultButtons).toMatchSnapshot()
+  })
+})
